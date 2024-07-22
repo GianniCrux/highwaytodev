54a750d0184a67c3c73f1591c1092e15ff6ce6fe
GianniCrux <gianni.disti@gmail.com>
2024-07-22 14:35:52 +0200

    implemented secret keys

--- firebase.ts
+++ firebase.ts
@@ -1,26 +1,26 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
+import { useEffect } from "react";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWoH3yCVq8dAxJxwQz7PkmmVadz2YNZeY",
-  authDomain: "highwaytodevs.firebaseapp.com",
-  databaseURL: "https://highwaytodevs-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "highwaytodevs",
-  storageBucket: "highwaytodevs.appspot.com",
-  messagingSenderId: "72562560223",
-  appId: "1:72562560223:web:96c6f4f6a6e4e055326d6a",
-  measurementId: "G-GX3VXSFTC9"
+  apiKey: process.env.REACT_API_KEY_FIREBASE_DB,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL, //cannot push the database, but with secret key url the app will not work. Modify this with the actual DatabaseUrl or it wont work! 
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const  database = getDatabase(app);
 
-const database = getDatabase(app)
 
 export { database }
