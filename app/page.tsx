81a845e99e65049ec44d8b94d456b5cc688aa6be
GianniCrux <gianni.disti@gmail.com>
2024-09-05 13:17:35 +0200

    fixed error and implemented AuthButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import AuthButton from "@/_components/authButton";
 import Game from "@/components/game";
 import styles from "@/components/Game.module.css"
+import "./globals.css"
 
 
 export default function Home() {
   return (
-    <main className="">
-      <h1> <span className={styles.title}>Highway To Dev</span> </h1>
+    <main>
+      <h1 className={styles.top}> <span className={styles.title}>Highway To Dev</span> </h1>
+      <AuthButton />
       <Game />
     </main>
   );
